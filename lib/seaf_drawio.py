c0f8df7fd989e6ff06d16f5d441c04867edc82ab
koa73 <koa73@bk.ru>
2025-03-11 23:13:49 +0300

    v. 1.0

---
+++ lib/seaf_drawio.py
@@ -0,0 +1,196 @@
+import sys
+import yaml
+import json
+import re
+from copy import deepcopy
+import xml.etree.ElementTree as ET
+
+class Seaf_Drawio:
+
+    def __init__(self, default_config):
+        """
+        Инициализация загрузчика конфигурации.
+        :param default_config: Словарь с конфигурацией по умолчанию.
+        """
+        self.default_config = default_config
+
+    def load_config(self, config_file):
+        """
+        Загружает конфигурацию из YAML-файла и объединяет её с конфигурацией по умолчанию.
+        :param config_file: Путь к YAML-файлу.
+        :return: Итоговая конфигурация.
+        """
+        try:
+            with open(config_file, 'r') as file:
+                user_config = yaml.safe_load(file) or {}
+        except FileNotFoundError:
+            print(f"Файл {config_file} не найден. Используются значения по умолчанию.")
+            user_config = {}
+
+        return self._merge_configs(deepcopy(self.default_config), user_config)
+
+    def _merge_configs(self, default, user):
+        """
+        Рекурсивно объединяет две конфигурации.
+        :param default: Конфигурация по умолчанию.
+        :param user: Пользовательская конфигурация.
+        :return: Объединённая конфигурация.
+        """
+        for key, value in user.items():
+            if isinstance(value, dict) and key in default:
+                self._merge_configs(default[key], value)
+            else:
+                default[key] = value
+        return default
+
+    @staticmethod
+    def read_object_file(file, **kwargs):
+        try:
+            with open(file, 'r') as file:
+                try:
+                    docs = yaml.safe_load_all(file)
+                    for doc in docs:
+                        return doc
+
+                except yaml.YAMLError as e:
+                    print("YAML error: {0}".format(e))
+
+        except IOError as e:
+            print("I/O error({0}): {1} : {2}".format(e.errno, e.strerror, file))
+            sys.exit(1)
+
+    @staticmethod
+    def append_to_dict(d, key, value):
+        try:
+            if value not in d[key]:
+                d[key].append(value)
+
+        except KeyError:
+            d[key] = []
+            d[key].append(value)
+
+    def find_key_value(self, data, target_key):
+        """
+        Recursively search for values associated with the given key in a nested JSON/dictionary.
+
+        :param data: The JSON/dictionary to search through.
+        :param target_key: The key to search for.
+        :return: A list of values associated with the target key.
+        """
+        results = []
+
+        # If the current data is a dictionary
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if key == target_key:
+                    results.append(value)  # Add the value if the key matches
+                if isinstance(value, (dict, list)):
+                    results.extend(self.find_key_value(value, target_key))  # Recurse into nested structures
+
+        # If the current data is a list
+        elif isinstance(data, list):
+            for item in data:
+                results.extend(self.find_key_value(item, target_key))  # Recurse into each item in the list
+
+        return results
+
+    def find_value_by_key(self, data, target_key):
+        """
+        Recursively search for a value by key in a nested dictionary.
+
+        :param data: The dictionary or list to search within
+        :param target_key: The key to search for
+        :return: The value associated with the target_key, or None if not found
+        """
+        if isinstance(data, dict):  # If the current item is a dictionary
+            if target_key in data:  # Check if the target_key exists in this dictionary
+                return data[target_key]
+            for value in data.values():  # Recursively search in the values of the dictionary
+                result = self.find_value_by_key(value, target_key)
+                if result is not None:
+                    return result
+        elif isinstance(data, list):  # If the current item is a list
+            for item in data:  # Recursively search in each item of the list
+                result = self.find_value_by_key(item, target_key)
+                if result is not None:
+                    return result
+        return None  # Return None if the key is not found
+
+    @staticmethod
+    def contains_object_tag(input_string, tag):
+        """
+                This function is useful when you need to quickly verify if a specific XML/HTML tag exists
+                at the beginning of a string. Checks whether a given string starts with an XML-like opening tag
+                that matches a specified tag name
+
+                :param input_string:
+                :param tag:
+                :return: True/False
+        """
+        pattern = rf"^<{tag}\b[^>]*>"
+
+        # Use re.search to check for the pattern
+        match = re.search(pattern, input_string)
+        return bool(match)
+
+    @staticmethod
+    def get_xml_pattern(xml, name):
+        """
+            Modify pattern to list of xml objects
+
+            :param xml: objects patten .
+            :param name: name of current pattern.
+            :return: list of objects.
+        """
+
+        wrapped_xml_string = f"<root>{xml}</root>"
+        result = []
+        try:
+            root = ET.fromstring(wrapped_xml_string)
+            for item in root:
+                result.append(ET.tostring(item, encoding='unicode'))
+            return result
+        except ET.ParseError as e:
+            print(f"Ошибка парсинга XML шаблона {name} : {e} ")
+
+            return result
+
+    @staticmethod
+    def list_contain(l, k):
+        """
+            Check if l (list) not empty and contain first value equal to string or in another lis
+
+            :param l: list.
+            :param k: string or list for comparing.
+            :return: boolean True/False.
+        """
+        if isinstance(k, str):
+            return True if len(l) > 0 and l[0] == k else False
+        elif isinstance(k, list):
+            return True if len(l) > 0 and l[0] in k else False
+
+    def get_object(self, file, key, **kwargs):
+        """
+            Get JSON leave from file by key
+
+            :param file: input file name.
+            :param key: key for finding sub JSON.
+            :param kwargs['type'] find json which contain value in key, kwargs['sort'] sorting by key
+            :return: json object.
+        """
+        x = json.loads(json.dumps(self.read_object_file(file)[key]))
+        if kwargs.get('type'):
+
+            if kwargs['type'].find(":") != -1:
+                k1, v1 = kwargs['type'].split(':')
+            else:
+                k1, v1 = 'type', kwargs['type']
+
+            r = {k2: v2 for k2, v2 in x.items() if self.list_contain(self.find_key_value(v2, k1), v1)}
+
+            if kwargs.get('sort'):
+                return dict(sorted(r.items(), key=lambda item: self.find_value_by_key(item[1], kwargs["sort"])))
+
+            return r
+        else:
+            return x
---
+++ seaf2drawio.py
@@ -0,0 +1,204 @@
+from N2G import drawio_diagram
+import sys
+import json
+import re
+import os
+from copy import deepcopy
+from lib import seaf_drawio
+
+patterns_dir = 'data/patterns/'
+diagram = drawio_diagram()
+node_xml_default = diagram.drawio_node_object_xml
+root_object = 'seaf.ta.services.dc_region'
+diagram_pages = {'main': ['Main Schema'], 'office': [], 'dc': []}
+diagram_ids = {'Main Schema':[]}
+conf = {}
+
+# Переменные по умолчанию
+DEFAULT_CONFIG = {
+    "seaf2drawio": {
+        "data_yaml_file": "data/example/test_seaf_ta_P41_v0.5.yaml",
+        "drawio_pattern": "data/base.drawio",
+        "output_file": "result/Sample_graph.drawio"
+    }
+}
+
+d = seaf_drawio.Seaf_Drawio(DEFAULT_CONFIG)
+
+def position_offset(pattern):
+
+    match pattern['algo']:
+        # По оси Y cверху вниз относительно родительского объекта
+        case 'Y+':
+            if return_ready(pattern):
+                pattern['x'] = pattern['x'] +  pattern['w'] + pattern['offset']
+                pattern['y'] = pattern['y'] - (pattern['h'] + pattern['offset']) * pattern['deep']
+            pattern['y'] = pattern['y'] + pattern['h'] + pattern['offset']
+
+        case 'Y-':
+            if return_ready(pattern):
+                pattern['x'] = pattern['x'] +  pattern['w'] + pattern['offset']
+                pattern['y'] = pattern['y'] + (pattern['h'] + pattern['offset']) * pattern['deep']
+            pattern['y'] = pattern['y'] - pattern['h'] - pattern['offset']
+
+        case 'X-':
+
+            if return_ready(pattern):
+                pattern['y'] = pattern['y'] +  pattern['h'] + pattern['offset']
+                pattern['x'] = pattern['x'] + (pattern['w'] + pattern['offset']) * pattern['deep']
+            pattern['x'] = pattern['x'] - pattern['w'] - pattern['offset']
+        # По оси X слева направо
+        case 'X+':
+            if return_ready(pattern):
+                pattern['y'] = pattern['y'] +  pattern['h'] + pattern['offset']
+                pattern['x'] = pattern['x'] - (pattern['w'] + pattern['offset']) * pattern['deep']
+            pattern['x'] = pattern['x'] + pattern['w'] + pattern['offset']
+
+def return_ready(pattern):
+    pattern['count']+=1
+    if pattern['count'] == pattern['deep']:
+        pattern['count'] = 0
+
+    return not bool(pattern['count'])
+
+def get_parent_value(pattern, current_parent):
+    r = ''
+    if pattern.get('parent_key'):
+        r = d.find_value_by_key(d.find_value_by_key(json.loads(json.dumps(d.read_object_file(conf['data_yaml_file']))),
+                                                current_parent), pattern['parent_key'])
+    return r
+
+def add_pages(pattern):
+
+    if pattern.get('ext_page'):
+
+        page_data = d.get_object(conf['data_yaml_file'], pattern['schema'])
+        diagram_xml_default = diagram.drawio_diagram_xml
+
+        for key_id in list( page_data.keys() ):
+
+            diagram.drawio_diagram_xml = pattern['ext_page']
+            diagram.add_diagram(key_id + '_page', page_data[key_id]['title'])
+
+            diagram_pages[k].append(page_data[key_id]['title'])
+            d.append_to_dict(diagram_ids, page_data[key_id]['title'], key_id)
+
+        diagram.drawio_diagram_xml = diagram_xml_default
+        diagram.go_to_diagram(page_name)
+
+def add_node_object(pattern, data, key_id):
+
+    #print(f'-------- {page_name} ------------------{key_id} --------------------------------')
+
+    pattern_count, current_parent = 0, ''
+    for xml_pattern in d.get_xml_pattern(pattern['xml'], key_id):
+
+        diagram.drawio_node_object_xml = xml_pattern
+
+        # Если у элемента есть родитель, получаем ID родителя и проверяем связан ли родитель с текущей диаграммой (страницей)
+        # добавляем в справочник ID элемента
+
+        if pattern.get('parent_id') and d.list_contain(d.find_key_value(data, pattern['parent_id']),
+                                                     diagram_ids[page_name]) and pattern_count == 0:
+            d.append_to_dict(diagram_ids, page_name, key_id)
+            current_parent = d.find_value_by_key(data, pattern['parent_id'])
+
+            if current_parent != pattern['last_parent']:   # reset to default pattern
+                default_pattern['parent'] = get_parent_value(pattern, current_parent)
+                pattern.update(default_pattern)
+                pattern['last_parent'] = current_parent
+
+        try:
+            diagram.drawio_node_object_xml = diagram.drawio_node_object_xml.format_map(
+                data | {'Group_ID': f'{key_id}_0', 'parent_type' : default_pattern['parent']})  # замена в xml шаблоне переменных в одинарных {}, добавление ID группы
+            data['OID'] = key_id
+
+        except KeyError as e:
+
+            #print("Can't add object: {id} to page: {page}. Key: {key} out of dictionary. Data: {data}"
+            #      .format(key=str(e), id=i, page=page_name, data=data))
+            return
+
+        if key_id in diagram_ids[page_name]:
+
+            data['schema'] = pattern['schema']
+            diagram.add_node(
+                id=f"{key_id}_{pattern_count}" if not d.contains_object_tag(xml_pattern, 'object') else key_id,
+                label=data['title'],
+                x_pos=pattern['x'],
+                y_pos=pattern['y'],
+                width=pattern['w'],
+                height=pattern['h'],
+                data=data if d.contains_object_tag(xml_pattern, 'object') else {},
+                url=pattern.get('ext_page') and data['title']
+            )
+            d.append_to_dict(diagram_ids, page_name, key_id)  # Добавляет ID root элементов
+
+            if pattern_count == 0:  # Change position of element
+                position_offset(object_pattern)
+
+            pattern_count += 1
+
+        diagram.drawio_node_object_xml = node_xml_default
+
+def add_links(pattern):
+
+    diagram.drawio_link_object_xml = pattern['xml']
+
+    for source_id, targets in d.get_object(conf['data_yaml_file'], pattern['schema'], type=object_pattern.get('type')).items():  # source_id - ID объекта
+        if source_id in diagram_ids[page_name]:  # Объект присутствует на текущей диаграмме
+            if pattern.get('parent_id'):
+                targets = {pattern['targets']: [get_parent_value(pattern, targets[pattern['parent_id']])]}
+            for target_id in targets[pattern['targets']]:
+                if target_id in diagram_ids[page_name]:  # Объект для связи присутствует на диаграмме
+                    diagram.add_link(source=source_id, target=target_id, style=pattern['style'])
+
+                else:
+                    print(f' Can\'t link  {source_id} <---> {target_id}, object {target_id} not found at the page '
+                          f'{page_name}')
+
+if __name__ == '__main__':
+
+    if sys.version_info < (3, 9):
+        print("Этот скрипт требует Python версии 3.9 или выше.")
+        sys.exit(1)
+
+    conf =  d.load_config("config.yaml")['seaf2drawio']
+
+    diagram.from_file(filename=conf['drawio_pattern'])
+    diagram_ids['Main Schema'] = list(d.get_object(conf['data_yaml_file'], root_object).keys())
+
+    for file_name, pages in diagram_pages.items():
+
+        for page_name in pages:
+
+            diagram.go_to_diagram(page_name)
+
+            for k, object_pattern in d.read_object_file(patterns_dir + file_name + '.yaml').items():
+
+                try:
+                    object_data = d.get_object(conf['data_yaml_file'], object_pattern['schema'], type=object_pattern.get('type'),
+                                             sort=object_pattern['parent_id'] if object_pattern.get('parent_id') else None)
+                    add_pages(object_pattern)
+                    object_pattern['count'] = 0  # Счетчик объектов
+                    object_pattern['last_parent'] = '' # Тригер для отслеживания изменения родительского объекта
+                    object_pattern['parent'] = ''
+                    default_pattern = deepcopy(object_pattern)
+
+                    for i in list(object_data.keys()):
+                        #print(f'i: {i}   ---  {object_data[i]}')
+                        if diagram._node_exists(id=i):
+                            diagram.update_node(id=i, data=object_data[i])
+                        else:
+                            add_node_object(object_pattern, object_data[i], i)
+
+                except KeyError as e:
+                    print("Object: {k} Page: {page} Key: {key} not found in data file".format(k=k,page=page_name,
+                                                                                        key=object_pattern['schema']))
+                    #break
+
+                if bool(re.match(r'^network_links(_\d+)*',k)):
+                    add_links(object_pattern)  # Связывание объектов на текущей диаграмме
+
+    diagram.dump_file(filename=os.path.basename(conf['output_file']), folder=os.path.dirname(conf['output_file'])
+    if os.path.dirname(conf['output_file']) else './')

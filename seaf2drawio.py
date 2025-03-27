fc90929d2eceb1248a57b3f7b5d32d0c000bd262
koa73 <koa73@bk.ru>
2025-03-27 22:17:02 +0300

    v. 1.2

--- seaf2drawio.py
+++ seaf2drawio.py
@@ -137,7 +137,8 @@ def add_object(pattern, data, key_id):
 
         try:
             diagram.drawio_node_object_xml = diagram.drawio_node_object_xml.format_map(
-                data | {'Group_ID': f'{key_id}_0', 'parent_type' : default_pattern['parent']})  # замена в xml шаблоне переменных в одинарных {}, добавление ID группы
+                data | {'Group_ID': f'{key_id}_0', 'parent_type' : default_pattern['parent'],
+                        'parent_id': data[pattern['parent_id']]})  # замена в xml шаблоне переменных в одинарных {}, добавление ID группы
             data['OID'] = key_id
 
         except KeyError as e:

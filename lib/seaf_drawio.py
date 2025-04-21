b8ec86f60205bd578fc8244420862a6dfd81f3bf
koa73 <koa73@bk.ru>
2025-04-21 18:01:19 +0300

    v. 1.2

--- lib/seaf_drawio.py
+++ lib/seaf_drawio.py
@@ -272,7 +272,7 @@ class SeafDrawio:
 
         # Исключение атрибутов 'id' и 'label'
         return {
-            attributes['schema']: {attributes['OID']: {key: html.unescape(value) for key, value in attributes.items()
+            attributes.get('schema'): {attributes.get('OID'): {key: html.unescape(value) for key, value in attributes.items()
                                                        if key not in ['id', 'label', 'OID', 'schema']}}}
 
     def get_data_from_diagram(self, file_name):
